3fef0135dc8124c46e5036f7da77b41cb36102ac
Adnan-127 <126506690+Adnan-127@users.noreply.github.com>
2024-02-19 15:21:57 +0300

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
   $(".flip-card-inner").click(function () {
     let imgSRC = $(this).children().children().attr("src");
+    let card = $(this);
 
+    $(card).toggleClass('flip-action');
     if ($(".container .choose-one .form-check-input").is(":checked")) {
       $(".container .character img").attr("src", imgSRC);
-      $(".container .choose-one .form-check-input").prop('checked', false);;
+      $(".container .choose-one .form-check-input").prop('checked', false);
+      $(card).removeClass('flip-action');
     }
+  });
 
-    $(this).toggleClass('flip-action');
+  $(".container .choose-theme .form-check-input").on("click", function () {
+    $("body,.container h1,.container,.flip-card-front,.flip-card-back,.container .choose-one .form-check-lable,.container .choose-theme .form-check-lable").toggleClass("dark");
   });
 });
